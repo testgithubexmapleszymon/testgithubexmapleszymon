abb58d0b53a896ca17991e2fe8e8d846dbde640d
Szymon WASIAK <Szymon.WASIAK@rbinternational.com.pl>
2022-11-10 12:54:09 +0100

    t

--- .github/actions/create_pr_automerge/create_pr_automerge.sh
+++ .github/actions/create_pr_automerge/create_pr_automerge.sh
@@ -1,4 +1,1 @@
-# OUTPUT=$(gh pr create --base=dev01 --header=main)
-# gh pr merge "${OUTPUT}" --merge --title "[AUTOMATED] Pull request to dev01" --body "[AUTOMATED] Pull request body"
-# gh pr create --base=dev01 --header=main
 echo "Hejo"
